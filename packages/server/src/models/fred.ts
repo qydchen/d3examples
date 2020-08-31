3c98f3f44fdb96dd22ce1c1b1e4c649bdacf1ea6
David Chen <qydchen@gmail.com>
2020-08-30 21:26:57 -0400

    dont pass req or rest object into business logic

--- packages/server/src/models/fred.ts
+++ packages/server/src/models/fred.ts
@@ -2,8 +2,8 @@ import { API_KEY } from '../../__secret__/fred-api';
 import fetch from 'node-fetch'
 const URL = `https://api.stlouisfed.org/fred/category?category_id=125&api_key=${API_KEY}&file_type=json`;
 
-export const get = async (cb: (arg: Body) => void) => {
+export const get = async () => {
     const response = await fetch(URL);
     const json = await response.json();
-    cb(json);
+    return json;
 }
--- packages/server/src/routes/fred.ts
+++ packages/server/src/routes/fred.ts
@@ -3,9 +3,10 @@ import * as Fred from '../models/fred';
 
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     // when res.send gets called later, it is divorced from its dynamic scope--which means that calls to this inside of the send method,
-    Fred.get(res.send.bind(res));
+    const result = await Fred.get();
+    return res.json(result);
 });
 
 export default router;
